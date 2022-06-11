01d67f29da286cb612432627fd73cf890df992cf
Olivia Adongo <akinyioliviatove@gmail.com>
2022-06-11 18:12:15 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+const result = drivers.filter(findMatching);
+function findMatching(){
+    
+}
+function fuzzyMatch(){}
+function matchName(){}
