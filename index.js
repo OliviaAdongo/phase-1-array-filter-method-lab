93f0e85b465aaadb89cf54f8c0a37c4ae4b5e6f9
Olivia Adongo <akinyioliviatove@gmail.com>
2022-06-13 08:03:12 +0300

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-// Code your solution here
-const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
-const result = drivers.filter(findMatching);
-function findMatching(){
+// // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+// const result = drivers.filter(findMatching);
+// function findMatching(){
     
+// }
+// function fuzzyMatch(){}
+// function matchName(){}
+function matchName(drivers, string){
+    return drivers.filter(driver =>
+        driver.name === string
+    )
 }
-function fuzzyMatch(){}
-function matchName(){}
+
+function findMatching(drivers, string){
+    return drivers.filter(driver => 
+          driver.toLowerCase() === string.toLowerCase()
+      )
+}
+  
+function fuzzyMatch(drivers, string){
+      return drivers.filter(driver =>
+          driver[0] === string[0]
+      )
+}
